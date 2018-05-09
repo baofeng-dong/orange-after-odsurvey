be99c8ebf75a48fc132df95c288fb99b8252ab44
Dong, Baofeng <dongb@trimet.org>
2018-05-09 15:54:26 -0700

    added metadata file for trips and demographics data

---
+++ dashboard/mod_long/metadata.py
@@ -0,0 +1,20 @@
+metadata = {
+    "trip": {
+        "1": "Work",
+        "2": "Other business",
+        "3": "Home",
+        "4": "College/University",
+        "5": "Airport,Amtrak/Greyhound",
+        "6": "Recreation",
+        "7": "Medical appointment",
+        "8": "Social visit",
+        "9": "Personal business",
+        "10": "Pick up/drop off",
+        "11": "Shopping",
+        "12": "Eating out",
+        "13": "School (K-12)",
+        "14": "Hotel",
+        "15": "Sporting event",
+        "16": "Other"
+    }
+}

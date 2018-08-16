464c4191e3a60cb62ced472d37806481cf8d178b
Dong, Baofeng <dongb@trimet.org>
2018-08-16 09:55:15 -0700

    updated files

--- dashboard/mod_onoff/helper.py
+++ dashboard/mod_onoff/helper.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2017 Baofeng Dong
-# This program is released under the "MIT License".
-# Please see the file COPYING in the source
-# distribution of this software for license terms.
-
 
 import csv, os
 
